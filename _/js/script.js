83ecc9781b93375d1a7e6bef7e110f3af05eb825
IegorKozakov <kozakov.e@gmail.com>
2014-06-17 02:40:51 +0300

    adding first files
    adding all files from PC to git

---
+++ _/js/script.js
@@ -0,0 +1,17 @@
+var wrapper_width = document.getElementById("wrapper").offsetWidth,
+	content =  document.getElementById("content");
+
+
+function content_hide () {
+		content.style.top = - + 100 + '%';
+		content.style['transition-duration'] = '0.8s';
+
+	}
+
+// ======================= END Hide Content div ======================= \\
+
+// ======================= Show nav menu =======================\\
+
+
+
+// =======================          ======================= \\
