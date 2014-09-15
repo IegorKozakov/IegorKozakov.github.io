1c2425d326e4b85c9f213e2e204f9bf9608b8cc8
kozakov.e@gmail.com <kozakov.e@gmail.com>
2014-09-16 00:06:14 +0300

    merge in html to index.html

--- sources/js/main.js
+++ sources/js/main.js
@@ -2,13 +2,40 @@ $( document ).ready(function() {
 	$('.main_page_intro').css('opacity', '1');
 });
 
+$('.home').click(function(){
+	location.reload();
+});
+
 $('.details-btn').click(function() {
 
 	$('.main_page_intro').css('top', '-999px');
 	
 	setTimeout(function () {
- 	window.location.href = "description.html"}, 1000 );
+ 	$('.intrio-slide-wrap').css('opacity', 0);
+ 	$('.description-slide').css('opacity', 1);
+ 	}, 1000 );
 
+	setTimeout(function () {
+		$('.content').show();
+		$('.nav-bock').css("top", "30px");
+	}, 1000);
+	
+	setTimeout(function () {
+		$('.intrio-slide-wrap').hide();
+	}, 2000);
+
+	setTimeout(rotate, 1000);
 
  	return false;
 });
+
+function rotate() {
+	$('.nav-bock').css({
+		'-moz-transform': 'rotate(0deg)', 
+		'-ms-transform': 'rotate(0deg)',
+		'-webkit-transform': 'rotate(0deg)',
+		'-o-transform': 'rotate(0deg)',
+		'transform': 'rotate(0deg)',
+		'top': '10px'
+	})
+}
